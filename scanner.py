bd273bc36fc0640f38f6184e909ccbcf830431b6
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2026-04-09 13:53:21 +0300

    feat: add threaded port scanner with socket

--- scanner.py
+++ scanner.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import time
 
 while True:
     while True:
@@ -20,5 +22,35 @@ while True:
             print("Invalid format. Use example: 1-1024")
 
 
+    #Main Scans
+
+    def scan_ports(sip, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1) 
+        result = s.connect_ex((sip, port))
+        if result == 0:
+            print(f"Port {port} is open")
 
     
+
+    # Start threads for each link
+    threads = []
+    for port in range(ps , pe+1):
+        # Using `args` to pass positional arguments and `kwargs` for keyword arguments
+        t = threading.Thread(target=scan_ports, args=(sip, port))
+
+        threads.append(t)
+        
+
+    # Start each thread
+    
+    print(f"Port scans started for {sip}")
+    for t in threads:
+        t.start()
+
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
+
+    print(f"Port scans ended for {sip}")
+            
